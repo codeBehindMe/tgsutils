5605215b17fa1965f1b7e66f59ed185a06385d5f
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-08-18 08:02:05 +1000

    Plot image histogram method to plot class

--- visualisation.py
+++ visualisation.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.graph_objs as go
+import plotly.plotly as py
 
 
 class Plot:
@@ -8,10 +10,23 @@ class Plot:
     def plot_image(img: np.ndarray, **kwargs) -> plt.Figure:
         """
         Plots an image.
-        :param img:
+        :param img: Image to be plotted.
+        :param kwargs: Additional arguments to plot.
         :return:
         """
 
         fig = plt.figure()
         plt.imshow(img, **kwargs)
         return fig
+
+    @staticmethod
+    def plot_image_histogram(img: np.ndarray, **kwargs) -> plt.Figure:
+        """
+        Return the histogram of pixel intensities.
+        :param img: Image to plot histogram of.
+        :param kwargs: Additional arguments.
+        :return:
+        """
+        flat = img.flatten()
+        data = [go.Histogram(x=flat, **kwargs)]
+        py.iplot(data, **kwargs)
