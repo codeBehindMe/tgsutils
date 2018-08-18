9b6fa25eec35bc94929b5d3197b95dac9dc6c01c
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-08-18 12:02:28 +1000

    Removed static classes and replaced with modules

--- visualisation.py
+++ visualisation.py
@@ -4,29 +4,26 @@ import plotly.graph_objs as go
 import plotly.offline as py
 
 
-class Plot:
+def plot_image(img: np.ndarray, **kwargs) -> plt.Figure:
+    """
+    Plots an image.
+    :param img: Image to be plotted.
+    :param kwargs: Additional arguments to plot.
+    :return:
+    """
 
-    @staticmethod
-    def plot_image(img: np.ndarray, **kwargs) -> plt.Figure:
-        """
-        Plots an image.
-        :param img: Image to be plotted.
-        :param kwargs: Additional arguments to plot.
-        :return:
-        """
+    fig = plt.figure()
+    plt.imshow(img, **kwargs)
+    return fig
 
-        fig = plt.figure()
-        plt.imshow(img, **kwargs)
-        return fig
 
-    @staticmethod
-    def plot_image_histogram(img: np.ndarray, **kwargs) -> plt.Figure:
-        """
-        Return the histogram of pixel intensities.
-        :param img: Image to plot histogram of.
-        :param kwargs: Additional arguments.
-        :return:
-        """
-        flat = img.flatten()
-        data = [go.Histogram(x=flat, **kwargs)]
-        return py.iplot(data)
+def plot_image_histogram(img: np.ndarray, **kwargs) -> plt.Figure:
+    """
+    Return the histogram of pixel intensities.
+    :param img: Image to plot histogram of.
+    :param kwargs: Additional arguments.
+    :return:
+    """
+    flat = img.flatten()
+    data = [go.Histogram(x=flat, **kwargs)]
+    return py.iplot(data)
